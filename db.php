1f67d6555d691caa425ed1373741a3c5dab67f9d
SayaPutih <129726398+SayaPutih@users.noreply.github.com>
2023-10-22 20:00:23 +0700

    Database

--- db.php
+++ db.php
@@ -1,3 +1,11 @@
 <?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "todo_list";
 
-define(DB);
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
